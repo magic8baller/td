5058df2b8fe5d7b57c33e652650b2268134f70a0
magic8baller <lturis@gmail.com>
2019-04-14 01:33:44 -0400

    ignoring

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import TodoInput from './Components/TodoInput';
+import TodoList from './Components/TodoList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container">
+          <div className="row">
+            <TodoInput />
+          </div>
+          <div className="row">
+            <TodoList />
+          </div>
+        </div>
       </div>
     );
   }
